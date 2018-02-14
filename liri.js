7674b63c4a4e53f9ea1705cf57b8e9f31d10ab1f
Amy <amysuelumpkin@gmail.com>
2018-02-14 18:17:51 -0500

    twitter works

--- liri.js
+++ liri.js
@@ -1,39 +1,43 @@
 require("dotenv").config();
-const keys = require("./keys.js"); //
+const keys = require("./keys.js");
 const request = require("request");
 const Twitter = require("twitter");
-let fs = require("fs");
+const Spotify = require("node-spotify-api");
+const fs = require("fs");
 
 const client = new Twitter(keys.twitter);
+const spotify = new Spotify(keys.spotify);
   //console.log(client);
 
-var liriSays = process.argv; //an attribute on an object
+//var liriSays = process.argv; //an attribute on an object
   //console.log(liriSays);
 
 var userFunction = process.argv[2];
   //console.log(userFunction);
 
+//----------TWITTER--------------------------
 switch (userFunction) {
   case "my-tweets":
   myTweets();
   break;
 default:
-  console.log("not valid, ask for my-tweets");
+  console.log("not a valid format, ask for 'my-tweets'");
 }
-
-//this is a function, only call in the case of my-tweets
+// still need them to show when they were created.
+//this is a function, only call in the case of 'my-tweets'
 function myTweets(){
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    //for loop here for 20 indexes.
-    console.log(tweets);
-
+  if (userFunction === "my-tweets"){
+  var params = {screen_name: 'ClicketyClyde', count: 20};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+      if (!error) {
+        console.log("These are your last 20 tweets: ");
+        tweets.forEach(p => console.log(`@${p.user.screen_name}: ${p.text} @ ${p.created_at}`));
+      }
+    });
   }
-});
-}
-//need to go through 20 indexes of the tweets array , and for each object console log the created_at and the text/body.
 
-//command: node liri.js my-tweets
-//show last 20 tweets
-//return either error or request
+//--------------SPOTIFY----------------------------
+// need a command for 'spotify-this-song'
+// node liri.js spotify-this-song '<song name here>'
+// command will display 4 properties
+// if no song is provided then have a default song
